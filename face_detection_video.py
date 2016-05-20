425c58f0bb45a280d7398a9fd3b609ecf2cb5ac3
unni332265 <unni332265@gmail.com>
2016-05-20 16:33:26 +0530

    added the functionality of side face detection

--- face_detection_video.py
+++ face_detection_video.py
@@ -2,7 +2,9 @@ import cv2
 import sys
 
 cascPath = '/home/unnirajendran/Desktop/face_detect/haarcascade_frontalface_default.xml'
+cascPath2 = '/home/unnirajendran/Desktop/face_detect/haarcascade_profileface.xml'
 faceCascade = cv2.CascadeClassifier(cascPath)
+faceCascade2 = cv2.CascadeClassifier(cascPath2)
 
 #give the name of the input video file
 
@@ -11,7 +13,7 @@ cap = cv2.VideoCapture('video.mp4')
 while (cap.isOpened()):
     # Capture frame-by-frame
     ret, frame =cap.read()
-
+    #convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = faceCascade.detectMultiScale(
@@ -23,9 +25,22 @@ while (cap.isOpened()):
         flags=0
     )
 
-    # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    faces2 = faceCascade2.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30),
+        # flags=cv2.cv.CV_HAAR_SCALE_IMAGE
+        flags=0
+    )
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 0), 2)
+        
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
