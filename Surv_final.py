c43a7f8d002195b05f4560b4392659877d648fb8
Arpit Solanki <solankiarpit1997@gmail.com>
2016-06-14 17:13:12 +0530

    added label on faces in output video

--- Surv_final.py
+++ Surv_final.py
@@ -1,4 +1,5 @@
 #import necessary packages
+from __future__ import print_function
 import cv2
 import os
 from imutils.object_detection import non_max_suppression
@@ -7,10 +8,13 @@ import sys
 import numpy as np
 from PIL import Image	
 import imutils
+import time	
 from imutils.object_detection import non_max_suppression
 
+subject_label = 1
 total_count = 0
 subject_one_count = 0
+font = cv2.FONT_HERSHEY_SIMPLEX
 list_of_videos = []
 #haar-cascade to detect faces
 cascade_path = "haarcascade_profileface.xml"
@@ -40,11 +44,15 @@ def detect_face(frame):
 ###################################################
 #function to recognize human faces using LBPH face recognizer and pretrained model file
 def recognize_face(frame_orginal,faces):
+	predict_label = []
+	predict_conf = []
 	for x,y,w,h in faces:
 		frame_orginal_grayscale = cv2.cvtColor(frame_orginal[y: y + h, x: x + w],cv2.COLOR_BGR2GRAY)
 		cv2.imshow("cropped",frame_orginal_grayscale)
 		predict_tuple = recognizer.predict(frame_orginal_grayscale)
-		predict_label,predict_conf = predict_tuple
+		a,b = predict_tuple
+		predict_label.append(a)
+		predict_conf.append(b)
 		print(predict_tuple)
 	return predict_label
 		# if predict_conf < 100.0:
@@ -60,6 +68,16 @@ def draw_faces(frame,faces):
 		yB = y+h
 		cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
 	return frame
+
+###################################################
+#function to put labels on faces predicted by LBPH face recognizer
+def put_label_on_face(frame,faces,labels):
+	i=0
+	for x,y,w,h in faces:
+		cv2.putText(frame,str(labels[i]),(x,y), font, 1,(255,255,255),2)
+		i = i+1
+	return frame
+
 ####################################################
 #main function
 if __name__=='__main__':	
@@ -73,6 +91,7 @@ if __name__=='__main__':
 		for video in list_of_videos:
 			camera = cv2.VideoCapture(os.path.join(path,video))
 			while True:
+				starttime = time.time()
 				grabbed,frame = camera.read()
 				if not grabbed:
 					break
@@ -81,19 +100,21 @@ if __name__=='__main__':
 				frame_processed = detect_people(frame_orginal1)
 				faces = detect_face(frame_orginal)
 				if len(faces) > 0:
-					# print("face detected")
 					frame_processed = draw_faces(frame_processed,faces)
 					label = recognize_face(frame_orginal,faces)
-					total_count = total_count+1
-					if label == 1:
-						subject_one_count = subject_one_count+1
+					frame_processed = put_label_on_face(frame_processed,faces,label)
+					for i in label:
+						total_count = total_count+1
+						if i == 1:
+							subject_one_count = subject_one_count+1
 				cv2.imshow("window",frame_processed)
 				key = cv2.waitKey(1) & 0xFF
 				if key == ord("q"):
 					break
 			camera.release()
 			cv2.destroyAllWindows()
+			endtime = time.time()
+			print("accuracy is ",subject_one_count*100/total_count)
 		# print("total_count is {} and subject one count is {}".format(total_count,subject_one_count))
-		print("accuracy is ",subject_one_count*100/total_count)
 	else:
 		print("model file not found")
