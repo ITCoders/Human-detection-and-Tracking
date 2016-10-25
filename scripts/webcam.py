bea72fa56fedf00dc5592e78f5583b94776f2ecd
the-zebulan <the-zebulan@users.noreply.github.com>
2016-10-24 21:06:01 -0600

    PEP8: missing whitespace, no newline at end of file

--- scripts/webcam.py
+++ scripts/webcam.py
@@ -23,7 +23,7 @@ while True:
 
     # Draw a rectangle around the faces
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
