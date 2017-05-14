c18b52afdca322d0129bb61847a8a7467f198ce1
prsntmaurya <prsntmaurya@gmail.com>
2017-05-15 04:02:31 +0530

    Added function for face detection

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ using namespace cv;
 String face_cascade1_name = "haarcascade_frontalface_default.xml";
 CascadeClassifier face_cascade1;
 
-void detect_faces( Mat frame );
+void detect_faces( Mat frame, double fps);
 
 int main(int argc, const char** argv)
 {
@@ -32,7 +32,6 @@ int main(int argc, const char** argv)
     double fps = cam.get(CV_CAP_PROP_FPS); /*getting frames per seconds*/
     cout << "frames per seconds :" << fps << endl;
 
-    namedWindow("InputVideo",CV_WINDOW_AUTOSIZE);
 
     while(1)
     {
@@ -44,11 +43,29 @@ int main(int argc, const char** argv)
     		break;
     	}
 
-    	imshow("InputVideo", frame);
-		waitKey(1000/fps); /*interval between two cosecutive frames*/
- 
+    	detect_faces(frame,fps);	 
     }
 
     return 0;
 }
 
+void detect_faces( Mat frame, double fps )
+{
+	std::vector<Rect> faces;
+    Mat frame_gray;
+    cvtColor( frame, frame_gray, COLOR_BGR2GRAY ); /*converting input image in grayscale form*/
+    equalizeHist( frame_gray, frame_gray ); 
+    
+    /*Detecting faces*/
+    face_cascade1.detectMultiScale( frame_gray, faces, 1.2, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+    for ( size_t i = 0; i < faces.size(); i++ )
+    {   
+        /*Drawing circle around faces*/ 
+        Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
+        ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2 ), 0, 0, 360, Scalar( 0, 250, 255 ), 4, 8, 0 );
+    }
+
+    namedWindow( "Detected faces", WINDOW_AUTOSIZE );
+    imshow( "Detected faces", frame );
+    waitKey(100/fps);   
+}
